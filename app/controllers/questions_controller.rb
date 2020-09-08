e55329eb25eaad58ace3241a656cfdc825f58ed9
Permz <sakanaquarium0mashim0@gmail.com>
2020-09-08 14:27:14 +0900

    users controller created

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,12 @@
 class QuestionsController < ApplicationController
+  def new
+    @question = Question.new
+  end
   def index
+    @question = Question.all
+  end
+  def show
+    @question = Question.find_by(params[:id])
   end
   def create
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   root "home#top"
   get "/" => "home#top"
   get "/about" => "home#about"
@@ -9,4 +11,6 @@ Rails.application.routes.draw do
   # post "home/guest_sign_in" => "home#new_guest"
   
   devise_for :users
+  resources :questions
+  resources :users
 end
