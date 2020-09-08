3ee41116502bfdc1b858ff4e287dc45c2c3464e6
Permz <sakanaquarium0mashim0@gmail.com>
2020-09-08 13:43:26 +0900

    importing top text

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :Questions
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true
+  validates :mail_address, presence: true
+  validates :mail_address, format: { with: VALID_EMAIL_REGEX }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "/techs" => "home#techs"
   get "/qanda" => "questions#index"
   get "/news" => "home#news"
+  get "/login" => "home#login"
   # post "home/guest_sign_in" => "home#new_guest"
   
   devise_for :users
