c354cdd637d2961f90ae54482ddacd59861e9184
Permz <sakanaquarium0mashim0@gmail.com>
2020-09-08 10:55:33 +0900

    devise installed ssuccessfully

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'questions/index'
+  devise_for :users
+  root "home#top"
   get "/" => "home#top"
   get "/about" => "home#about"
   get "/techs" => "home#techs"
   get "/qanda" => "home#qanda"
   get "/news" => "home#news"
+  get 'questions/index'
   
 end
