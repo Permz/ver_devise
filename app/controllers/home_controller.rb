86a0818c2fdc21f02d0812d7bb9af42519e51a5f
Permz <sakanaquarium0mashim0@gmail.com>
2020-09-08 11:33:42 +0900

    guest user log-in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,4 +9,11 @@ class HomeController < ApplicationController
   end
   def news
   end
+  def new_guest
+    user = User.find_or_create_by(email: "guest@example.com") do |u|
+      u.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user
+    redirect_to root_path, notice: "ゲストユーザーとしてログインしました！"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "/qanda" => "home#qanda"
   get "/news" => "home#news"
   get 'questions/index'
+  post "home/guest_sign_in" => "home#new_guest"
   
   devise_for :users
 end
