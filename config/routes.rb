15f11cba2eadeca18ea04e5f525ad150e14c3139
Permz <sakanaquarium0mashim0@gmail.com>
2020-09-08 12:02:38 +0900

    log-in successed

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ Rails.application.routes.draw do
   get "/" => "home#top"
   get "/about" => "home#about"
   get "/techs" => "home#techs"
-  get "/qanda" => "home#qanda"
+  get "/qanda" => "questions#index"
   get "/news" => "home#news"
-  get 'questions/index'
-  post "home/guest_sign_in" => "home#new_guest"
+  # post "home/guest_sign_in" => "home#new_guest"
   
   devise_for :users
 end
