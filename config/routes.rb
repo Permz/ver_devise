7ca46935343b816aac81212d74cb06edc9aa258c
Permz <sakanaquarium0mashim0@gmail.com>
2020-09-12 14:59:42 +0900

    devise:controllers users was generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  root "home#top"
   get "/" => "home#top"
   get "/about" => "home#about"
   get "/techs" => "home#techs"
   get "/qanda" => "home#qanda"
   get "/news" => "home#news"
   
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+}
+
 end
