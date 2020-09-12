101774871d9eed16e17f572a17aa912888831f56
Permz <sakanaquarium0mashim0@gmail.com>
2020-09-12 22:25:46 +0900

    scaffold answers created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :answers
   root "home#top"
   get "/" => "home#top"
   get "/about" => "home#about"
@@ -7,6 +8,10 @@ Rails.application.routes.draw do
   get "/qanda" => "home#qanda"
   get "/news" => "home#news"
   
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
